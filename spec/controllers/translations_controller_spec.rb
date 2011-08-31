5004505a65d44a31764d5aab33cfd660bb7f76b4
perry3819 <perry@hertler.org>
2011-08-30 17:08:37 -0700

    adding translations and locale helper methods

--- spec/controllers/translations_controller_spec.rb
+++ spec/controllers/translations_controller_spec.rb
@@ -1,5 +1,36 @@
 require 'spec_helper'
 
 describe TranslationsController do
+  describe "locale helper method" do
+    it "should use default en if not speicified in session or params" do
+      @controller.send(:locale).should == 'en'
+    end
 
+    it "should use session locale if it exists" do
+      session[:locale] = 'jo-locale'
+      @controller.send(:locale).should == 'jo-locale'
+    end
+
+    it "should use params locale as highest priority" do
+      session[:locale] = 'jo-locale'
+      get :index, :locale => 'params-locale'
+      @controller.send(:locale).should == 'params-locale'
+    end
+  end
+
+  describe "translations" do
+    it "should find all of the translations by locale" do
+      translations = [Translation.new]
+      Translation.should_receive(:find).with(:locale => 'en').and_return translations
+
+      @controller.send(:translations).should == translations
+    end
+  end
+
+  describe "get index" do
+    it "should render index template" do
+      get :index
+      response.should render_template(:index)
+    end
+  end
 end
