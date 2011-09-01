a33fc5ecb680c9cd2bf4a6f53d8698138218f389
perry3819 <perry@hertler.org>
2011-08-31 19:06:41 -0700

    submit form on locale change

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,20 +2,28 @@
 // Add new JavaScript/Coffee code in separate files in this directory and they'll automatically
 // be included in the compiled file accessible from http://example.com/assets/application.js
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// the compiled file.
-//
-//= require jquery
-//= require jquery-ui
-//= require jquery_ujs
-//= require ../../../vendor/assets/javascripts/externals
-//= require_tree .
+  // the compiled file.
+  //
+  //= require jquery
+  //= require jquery-ui
+  //= require jquery_ujs
+  //= require ../../../vendor/assets/javascripts/externals
+  //= require_tree .
 
-$(function() {
+  $(function() {
+    $(document).ready(function() {
+      $('.editable').inlineEdit({
+        save: function(e, data) {
+          return confirm('Change name to '+ data.value +'?');
+        }
+      });
 
-  $('.editable').inlineEdit({
-    save: function(e, data) {
-      return confirm('Change name to '+ data.value +'?');
+      $('select#locale').observeLocale();
+    });
+    $.fn.observeLocale = function() {
+      $(this).change(function() {
+        $(this).parents('form').submit();
+      });
     }
   });
-});
 
