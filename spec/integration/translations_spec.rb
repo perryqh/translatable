3f3e31a733caa61c4f8a91c223f855507c330938
perry3819 <perry@hertler.org>
2011-09-07 08:38:18 -0700

    better integration tests around the initial 'get' and key creation

--- spec/integration/translations_spec.rb
+++ spec/integration/translations_spec.rb
@@ -1,12 +1,27 @@
 describe "Translations" do
+  before(:each) do
+    Translation.send(:store).flushdb
+  end
+
   describe "GET /" do
-    it "displays i18n locale filtering" do
+    before(:each) do
       visit translations_url
+    end
+
+    it "displays i18n locale filtering" do
       page.should have_content("Locales")
+      page.should have_css("input#filter_by[type=text]")
+    end
+
+    it "displays create key form" do
+      within(:css, 'form.new_translation') {
+        page.should have_css("label[for=translation_key]")
+        page.should have_css("label[for=translation_value]")
+        page.should have_css("input[type=submit]")
+      }
     end
 
     it "displays key/value grid" do
-      visit translations_url
       page.should have_content("I18n Key")
       page.should have_content("Value")
     end
@@ -19,8 +34,13 @@ describe "Translations" do
       fill_in 'Value', :with => 'tranvalue'
       click_button 'Create'
 
-      page.should have_content 'trankey'
-      page.should have_content 'tranvalue'
+      find_field('Key').value.should be_blank
+      find_field('Value').value.should be_blank
+
+      within(:css, 'tbody tr') {
+        find('td.key').text.should == 'trankey'
+        find('td.value span.editable').text.should == 'tranvalue'
+      }
     end
   end
 end
