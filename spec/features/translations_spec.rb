43b48dfd4ee52b133cca2229a2dc3b4ca7f69f1e
Chris Stringer <chris.stringer@g5searchmarketing.com>
2013-02-14 10:18:39 -0800

    Refactor translations integration tests
    Moved to features dir as per new version of Capybara. Also changed
    matchers as the within syntax was failing for odd reasons. Lastly
    modified the test that uses Selenium to use a local Firefox instance
    instead of going out to Perry's old
    http://translatable-qa.herokuapp.com/ app sitting on Heroku.

---
+++ spec/features/translations_spec.rb
@@ -0,0 +1,70 @@
+require 'spec_helper'
+
+describe "Translations" do
+
+  before(:each) do
+    Translation.send(:store).flushdb
+  end
+
+  describe "GET /" do
+    before(:each) do
+      visit translations_path
+    end
+
+    it "displays i18n locale filtering" do
+      page.should have_content("Locales")
+      page.should have_css("input#filter_by[type=text]")
+    end
+
+    it "displays create key form" do
+      within(:css, 'form.new_translation') {
+        page.should have_css("label[for=translation_key]")
+        page.should have_css("label[for=translation_value]")
+        page.should have_css("input[type=submit]")
+      }
+    end
+
+    it "displays key/value grid" do
+      page.should have_content("I18n Key")
+      page.should have_content("Value")
+    end
+  end
+
+  describe "update translation" do
+    before(:each) do
+      @key = "foo"
+      @locale = 'en-US'
+      Translation.new(:locale => @locale, :key => @key, :value => 'bar').save
+      visit translations_path(:locale => @locale)
+      page.should have_css("tr[data-key=#{@key}]")
+    end
+
+    # Note that this test actually fires up Selenium and requires you to have Firefox installed
+    it "updates the translation's value", :js => true do
+      within(:css, "tr[data-key=#{@key}]") {
+        find('span.editable').click
+        find('input[type=text]').set('baz')
+        find('button.save').click
+      }
+      page.driver.browser.switch_to.alert.accept
+      visit translations_path(:locale => @locale)
+
+      find("tbody tr[data-key=#{@key}] td.value span.editable").should have_text("baz")
+    end
+  end
+
+  describe "create translation" do
+    it "should create valid translation" do
+      visit translations_path
+      fill_in 'Key', :with => 'trankey'
+      fill_in 'Value', :with => 'tranvalue'
+      click_button 'Create'
+
+      find_field('Key').value.should be_blank
+      find_field('Value').value.should be_blank
+
+      find("tbody tr td.key").should have_text("trankey")
+      find("tbody tr td.value span.editable").should have_text("tranvalue")
+    end
+  end
+end
