41584b70a8da0e15c1c691ea5f468999bab84cee
maeve <maeve.revels@getg5.com>
2013-08-06 11:24:11 -0700

    Convert all translation input into valid UTF-8
    Unfortunately, this introduced chattiness in the test suite.
    Something is printing "error : string is not in UTF-8" to
    STDOUT during feature specs around cp1252 encoded input. I suspect
    it's one of the test gems.

--- spec/controllers/translations_controller_spec.rb
+++ spec/controllers/translations_controller_spec.rb
@@ -35,21 +35,80 @@ describe TranslationsController do
   end
 
   describe "post create" do
-    before(:each) do
-      Translation.send(:store).flushdb
+    subject(:create) do
+      xhr :post, :create, translation: {key: key, value: value, locale: locale}
     end
 
-    it "should create translation if valid" do
-      xhr :post, :create, :translation => {:key => 'create.key', :value => 'value for you', :locale => 'ck-KK'}
+    before(:each) { Translation.send(:store).flushdb }
 
-      response.should redirect_to(translations_url)
-      Translation.locale_value('ck-KK', 'create.key').should == 'value for you'
+    let(:key) { 'create.key' }
+    let(:value) { 'value for you' }
+    let(:locale) { 'ck-KK' }
+
+    context 'when translation is valid ASCII' do
+      it 'should redirect to the translations index' do
+        create
+        response.should redirect_to(translations_url)
+      end
+
+      it 'should create the translation' do
+        create
+        expect(Translation.locale_value(locale, key)).to eq(value)
+      end
     end
 
-    it "should return 500 and not save if not valid" do
-      xhr :post, :create, :translation => {:key => '', :value => 'value for you', :locale => 'ck-KK'}
+    context 'when translation key is invalid' do
+      let(:key) { '' }
 
-      response.should render_template :index
+      it 'should return 500' do
+        create
+        expect(response.status).to eq(500)
+      end
+
+      it 'should render the translation index' do
+        create
+        expect(response).to render_template(:index)
+      end
+
+      it 'should display an error message' do
+        create
+        expect(flash[:error]).to eq('Error saving invalid translation.')
+      end
+
+      it 'should not save the translation' do
+        create
+        expect(Translation.locale_value(locale, key)).to_not be
+      end
+    end
+
+    context 'when translation key is not UTF-8' do
+      let(:key) { "\x91key\x92".force_encoding('cp1252') }
+
+      it 'should redirect to the translations index' do
+        create
+        response.should redirect_to(translations_url)
+      end
+
+      it 'should create the translation with a UTF-8 string as the key' do
+        create
+        utf8_encoded_key = "\u2018key\u2019".force_encoding('utf-8')
+        expect(Translation.locale_value(locale, utf8_encoded_key)).to eq(value)
+      end
+    end
+
+    context 'when translation value is not UTF-8' do
+      let(:value) { "testing \x97 value".force_encoding('cp1252') }
+
+      it 'should redirect to the translations index' do
+        create
+        response.should redirect_to(translations_url)
+      end
+
+      it 'should create the translation with a UTF-8 string as the value' do
+        create
+        utf8_encoded_value = "testing \u2014 value".force_encoding('utf-8')
+        expect(Translation.locale_value(locale, key)).to eq(utf8_encoded_value)
+      end
     end
   end
 
@@ -68,23 +127,63 @@ describe TranslationsController do
   end
 
   describe "put update" do
+    subject(:update) do
+      xhr :put, :update, id: translation_id, key: key, value: value
+    end
+
     before(:each) do
       Translation.send(:store).flushdb
-      Translation.new(:locale => 'en-US', :key => 'error', :value => 'big error!').save
-      Translation.locale_value('en-US', 'error').should == 'big error!'
+      Translation.new(locale: locale, key: key, value: orig_value).save
     end
 
-    it "should update key" do
-      xhr :put, :update, :id => 'yada', :key => 'error', :value => 'even bigger'
+    let(:locale) { 'en-US' }
+    let(:translation_id) { 'yada' }
+    let(:key) { 'error' }
+    let(:orig_value) { 'big error!' }
 
-      response.should be_success
-      Translation.locale_value('en-US', 'error').should == 'even bigger'
+    context 'when value is valid ASCII' do
+      let(:value) { 'even bigger' }
+
+      it 'should respond successfully' do
+        update
+        expect(response).to be_success
+      end
+
+      it 'should update the stored translation value' do
+        update
+        expect(Translation.locale_value(locale, key)).to eq(value)
+      end
     end
 
-    it "should return 500 and not save if key not found" do
-      xhr :put, :update, :id => 'xx', :key => '', :value => 'value for you', :locale => 'ck-KK'
+    context 'when translation does not exist' do
+      let(:translation_id) { 'xx' }
+      let(:key) { '' }
+      let(:value) { 'should never happen' }
 
-      response.status.should == 500
+      it 'should return 500' do
+        update
+        expect(response.status).to eq(500)
+      end
+
+      it 'should not save translation' do
+        update
+        expect(Translation.locale_value(locale, key)).to be_nil
+      end
+    end
+
+    context 'when new value is not UTF-8' do
+      let(:value) { "testing \x97 value".force_encoding('binary') }
+
+      it 'should response successfully' do
+        update
+        expect(response).to be_success
+      end
+
+      it 'should update the stored translation value' do
+        utf8_encoded_value = "testing \u2014 value".force_encoding('utf-8')
+        update
+        expect(Translation.locale_value(locale, key)).to eq(utf8_encoded_value)
+      end
     end
   end
 end
--- spec/features/translations_spec.rb
+++ spec/features/translations_spec.rb
@@ -31,6 +31,14 @@ describe "Translations" do
   end
 
   describe "update translation" do
+    def update_value(value)
+      within(:css, "tr[data-key=#{@key}]") {
+        find('span.editable').click
+        find('input[type=text]').set(value)
+        find('button.save').click
+      }
+    end
+
     before(:each) do
       @key = "foo"
       @locale = 'en-US'
@@ -40,29 +48,48 @@ describe "Translations" do
     end
 
     it "updates the translation's value", :js => true do
-      within(:css, "tr[data-key=#{@key}]") {
-        find('span.editable').click
-        find('input[type=text]').set('baz')
-        find('button.save').click
-      }
+      update_value('baz')
       visit translations_path(:locale => @locale)
-
       find("tbody tr[data-key=#{@key}] td.value span.editable").should have_text("baz")
+    end
+
+    it "replaces invalid encoded characters", :js => true do
+      windows_encoded_value = "\x93quoted \x97 value\x94".force_encoding('cp1252')
+      utf8_encoded_value = "\u201cquoted \u2014 value\u201d".force_encoding('utf-8')
+      update_value(windows_encoded_value)
+      visit translations_path(:locale => @locale)
+      find("tbody tr[data-key=#{@key}] td.value span.editable").should have_text(utf8_encoded_value)
     end
   end
 
   describe "create translation" do
-    it "should create valid translation" do
+    def create_translation(key, value)
       visit translations_path
-      fill_in 'Key', :with => 'trankey'
-      fill_in 'Value', :with => 'tranvalue'
+      fill_in 'Key', :with => key
+      fill_in 'Value', :with => value
       click_button 'Create'
+    end
+
+    it "should create valid translation" do
+      create_translation('trankey', 'tranvalue')
 
       find_field('Key').value.should be_blank
       find_field('Value').value.should be_blank
 
       find("tbody tr td.key").should have_text("trankey")
       find("tbody tr td.value span.editable").should have_text("tranvalue")
+    end
+
+    it "should replace invalid encoded characters" do
+      windows_encoded_key = "\x91key\x92".force_encoding('cp1252')
+      utf8_encoded_key = "\u2018key\u2019".force_encoding('utf-8')
+
+      windows_encoded_value = "testing \x97 value".force_encoding('cp1252')
+      utf8_encoded_value = "testing \u2014 value".force_encoding('utf-8')
+      create_translation(windows_encoded_key, windows_encoded_value)
+
+      find("tbody tr td.key").should have_text(utf8_encoded_key)
+      find("tbody tr td.value span.editable").should have_text(utf8_encoded_value)
     end
   end
 end
